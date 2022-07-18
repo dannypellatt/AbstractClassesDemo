7d5abf3701497e129cfd521fa09d4331022ad865
dannypellatt <danielpellatt92@gmail.com>
2022-07-18 14:42:27 -0400

    As much of the exercise as I could

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Motorcycle
+    {
+        public Motorcycle()
+        {
+        }
+    }
+}
+
---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ConsoleUI
+{
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+        }
+    }
+}
+
